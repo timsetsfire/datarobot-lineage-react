876620b71d0378c03f78deb05a6ef105efd6a9d9
Timothy Whittaker <timsetsfire@gmail.com>
2025-04-10 10:21:31 -0400

    Update gdb.js

--- utils/gdb.js
+++ utils/gdb.js
@@ -20,32 +20,3 @@ async function getOrCreateNeo4jGraph() {
     }
     return neo4jGraph;
 }
-
-
-import "neo4j-driver";
-import { Neo4jGraph } from "@langchain/community/graphs/neo4j_graph";
-
-const url = process.env.NEO4J_URI;
-const username = process.env.NEO4J_USER;
-const password = process.env.NEO4J_PASSWORD;
-const graph = await Neo4jGraph.initialize({ url, username, password });
-
-// Import movie information
-const moviesQuery = `LOAD CSV WITH HEADERS FROM 
-'https://raw.githubusercontent.com/tomasonjo/blog-datasets/main/movies/movies_small.csv'
-AS row
-MERGE (m:Movie {id:row.movieId})
-SET m.released = date(row.released),
-    m.title = row.title,
-    m.imdbRating = toFloat(row.imdbRating)
-FOREACH (director in split(row.director, '|') | 
-    MERGE (p:Person {name:trim(director)})
-    MERGE (p)-[:DIRECTED]->(m))
-FOREACH (actor in split(row.actors, '|') | 
-    MERGE (p:Person {name:trim(actor)})
-    MERGE (p)-[:ACTED_IN]->(m))
-FOREACH (genre in split(row.genres, '|') | 
-    MERGE (g:Genre {name:trim(genre)})
-    MERGE (m)-[:IN_GENRE]->(g))`;
-
-await graph.query(moviesQuery);
